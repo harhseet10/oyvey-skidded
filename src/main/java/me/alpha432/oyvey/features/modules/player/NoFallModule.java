3acfe5327577e61ee7a3fc8c14aab14f5aebadf5
harhseet10 <bastolaharsheet@gmail.com>
2026-02-10 22:00:03 -0500

    Update NoFallModule.java

--- src/main/java/me/alpha432/oyvey/features/modules/player/NoFallModule.java
+++ src/main/java/me/alpha432/oyvey/features/modules/player/NoFallModule.java
@@ -6,7 +6,7 @@ import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
 
 public class NoFallModule extends Module {
     public NoFallModule() {
-        super("NoFall", "Removes fall damage", Category.PLAYER);
+        super("nomissinconvienience", "Removes Inconveinience", Category.PLAYER);
     }
 
     @Override
